b40907aaf19299cc9f7354d68ab3d1c09eac68ab
zverok <zverok.offline@gmail.com>
2020-02-09 14:27:37 +0200

    Stemmer works

--- spyll/hunspell/algo/stemmer.py
+++ spyll/hunspell/algo/stemmer.py
@@ -1,12 +1,31 @@
 import re
 import itertools
+import collections
+
+Result = collections.namedtuple('Result', ['stem', 'prefix', 'suffix'], defaults=[None, None])
 
 class Stemmer:
     def __init__(self, suffixes=[], prefixes=[]):
         self.suffixes = {
             flag: list(sufs) for flag, sufs in itertools.groupby(suffixes, key=lambda s: s.flag)
         }
-        self.prefixes = prefixes
+        self.prefixes = {
+            flag: list(prefs) for flag, prefs in itertools.groupby(prefixes, key=lambda s: s.flag)
+        }
+
+    def __call__(self, word):
+        res = [Result(word)] # "Whole word" is always existing option
+        for stem, suf in self.desuffix(word):
+            res.append(Result(stem, suffix=suf))
+
+        for stem, pref in self.deprefix(word):
+            res.append(Result(stem, prefix=pref))
+            if pref.crossproduct:
+                for stem2, suf in self.desuffix(stem):
+                    if suf.crossproduct:
+                        res.append(Result(stem2, prefix=pref, suffix=suf))
+
+        return res
 
     def desuffix(self, word):
         res = []
@@ -17,5 +36,17 @@ class Stemmer:
                     if re.search(suf.condition + '$', stem):
                         res.append((stem, suf))
                         break # only one variant of a suffix should be found
+
+        return res
+
+    def deprefix(self, word):
+        res = []
+        for flag, prefs in self.prefixes.items():
+            for pref in prefs:
+                if word.startswith(pref.add):
+                    stem = pref.strip + word[len(pref.add):]
+                    if re.search('^' + pref.condition, stem):
+                        res.append((stem, pref))
+                        break # only one variant of a prefix should be found
 
         return res
--- tests/hunspell/test_stemmer.py
+++ tests/hunspell/test_stemmer.py
@@ -1,6 +1,7 @@
 from spyll.hunspell.data import aff
 
 from spyll.hunspell.algo import Stemmer
+from spyll.hunspell.algo import stemmer as st
 
 def test_desuffix():
     suffixes = [
@@ -36,4 +37,59 @@ def test_desuffix():
     assert stemmer.desuffix('lens') == [
         ('len', aff.Suffix(flag='S', crossproduct=False, strip='', add='s', condition='[^sxzhy]')),
         ('l', aff.Suffix(flag='X', crossproduct=True, strip='', add='ens', condition='[^ey]')),
+    ]
+
+def test_depreffix():
+    # TODO: test it better. Current test data is taken from en_US.aff, and English is not very
+    # fancy around suffixes...
+    prefixes = [
+        aff.Prefix(flag='S', crossproduct=False, strip='', add='un', condition='.'),
+    ]
+    stemmer = Stemmer(prefixes=prefixes)
+
+    assert stemmer.deprefix('cat') == []
+    assert stemmer.deprefix('uncat') == [
+        ('cat', aff.Prefix(flag='S', crossproduct=False, strip='', add='un', condition='.')),
+    ]
+
+def test_call():
+    suffixes = [
+        aff.Suffix(flag='S', crossproduct=False, strip='y', add='ies', condition='[^aeiou]y'),
+        aff.Suffix(flag='S', crossproduct=False, strip='', add='s', condition='[aeiou]y'),
+        aff.Suffix(flag='S', crossproduct=False, strip='', add='es', condition='[sxzh]'),
+        aff.Suffix(flag='S', crossproduct=False, strip='', add='s', condition='[^sxzhy]'),
+
+        aff.Suffix(flag='D', crossproduct=True, strip='', add='d', condition='e')
+    ]
+    prefixes = [
+        aff.Prefix(flag='S', crossproduct=True, strip='', add='un', condition='.'),
+    ]
+    stemmer = Stemmer(suffixes=suffixes, prefixes=prefixes)
+
+    assert stemmer('cat') == [
+        st.Result('cat')
+    ]
+
+    # Only suffix exists
+    assert stemmer('cats') == [
+        st.Result('cats'),
+        st.Result('cat', suffix=aff.Suffix(flag='S', crossproduct=False, strip='', add='s', condition='[^sxzhy]'))
+    ]
+
+    # Prefix and suffix exist, but they are NOT combinable:
+    assert stemmer('uncats') == [
+        st.Result('uncats'),
+        st.Result('uncat', suffix=aff.Suffix(flag='S', crossproduct=False, strip='', add='s', condition='[^sxzhy]')),
+        st.Result('cats', prefix=aff.Prefix(flag='S', crossproduct=True, strip='', add='un', condition='.'))
+    ]
+
+    # Prefix and suffix, combinable
+    assert stemmer('unsaved') == [
+        st.Result('unsaved'),
+        st.Result('unsave', suffix=aff.Suffix(flag='D', crossproduct=True, strip='', add='d', condition='e')),
+        st.Result('saved', prefix=aff.Prefix(flag='S', crossproduct=True, strip='', add='un', condition='.')),
+        st.Result('save',
+            prefix=aff.Prefix(flag='S', crossproduct=True, strip='', add='un', condition='.'),
+            suffix=aff.Suffix(flag='D', crossproduct=True, strip='', add='d', condition='e')
+        )
     ]
