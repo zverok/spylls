d040d878b41a6f0250a7aa1b91812109e7aa127e
zverok <zverok.offline@gmail.com>
2020-11-28 12:19:44 +0200

    Prepare package

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="spyll",
+    version="0.0.1",
+    author="Victor Shepelev",
+    author_email="zverok.offline@gmail.com",
+    description="Hunspell ported to pure Python",
+    long_description=long_description,
+    url="https://github.com/spyll/spyll",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+
+        "License :: OSI Approved :: MIT License",
+
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+
+        "Operating System :: OS Independent",
+
+        "Topic :: Text Processing :: Linguistic"
+    ],
+    python_requires='>=3.7',
+    keywords=["hunspell", "spelling", "spellcheck", "suggest"]
+)
