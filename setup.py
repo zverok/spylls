07b93dfeb7c5ccf6008e1a24c7888af78239837b
zverok <zverok.offline@gmail.com>
2020-11-28 22:37:21 +0200

    Finalize the dist

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="spyll",
-    version="0.0.1",
+    name="spylls",
+    version="0.1.0",
     author="Victor Shepelev",
     author_email="zverok.offline@gmail.com",
     description="Hunspell ported to pure Python",
     long_description=long_description,
-    url="https://github.com/spyll/spyll",
+    url="https://github.com/zverok/spylls",
     packages=setuptools.find_packages(),
     classifiers=[
         "Development Status :: 4 - Beta",
